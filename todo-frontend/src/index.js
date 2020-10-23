a9a070e72c769519cb0ec1c10a45d09cb95e4cbb
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-10-23 14:57:45 -0500

    Added front end project structure

--- todo-frontend/src/index.js
+++ todo-frontend/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/App';
+import combinedReducers from './reducers/index';
+
+const store = createStore(combinedReducers, {
+  books: [],
+  filter: 'All',
+}, applyMiddleware(thunk));
+
+store.dispatch(fetchBooksAsync());
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
